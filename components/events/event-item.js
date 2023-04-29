50a05f4b1b57526ef9deb124dc56407fa845c079
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2023-04-29 16:28:43 -0500

    added in Image component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,6 +3,7 @@ import ArrowRightIcon from "../icons/arrow-right-icon";
 import DateIcon from "../icons/date-icon";
 import Button from "../ui/button";
 import classes from './event-item.module.css';
+import Image from "next/image";
 
 function EventItem({ title, image, date, location, id }) {
 
@@ -18,7 +19,12 @@ function EventItem({ title, image, date, location, id }) {
 
     return (
         <li className={classes.item}>
-            <img src={'/' + image} alt={title} />
+            <Image 
+                src={'/' + image}
+                alt={title}
+                width={250}
+                height={160}
+            />
             <div className={classes.content}>
                 <div className={classes.summary}>
                     <h2>{ title }</h2>
